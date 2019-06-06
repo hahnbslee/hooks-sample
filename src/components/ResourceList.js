53cc1f62b1732f77b54c26816914ce49a34ea7f7
Hahnbee Lee <hl985@cornell.edu>
2019-06-06 13:15:46 -0400

    up to vid 322

---
+++ src/components/ResourceList.js
@@ -0,0 +1,18 @@
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+
+
+const ResourceList = async ({resource}) => {
+    const [resources, setResources] = useState([]);
+    const fetchResource = async () => {
+        const response = await axios.get(`https://jsonplaceholder.typicode.com/${resource}`); 
+        setResources(response.data);
+    }
+
+    useEffect(() => {
+        fetchResource(resource);
+    }, [])
+    return (<div>{resources.length}</div>);
+}
+
+export default ResourceList;
